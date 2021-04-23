781d6bb29eb901e5894d5036f7ab793d38c5e98c
Alex Trost <alexrtrost@gmail.com>
2021-04-23 11:47:41 -0400

    Add component stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ["../slices/**/*.stories.[tj]s"],
+  stories: ["../slices/**/*.stories.[tj]s", "../components/**/*.stories.[tj]s"],
   addons: ["@storybook/preset-scss"],
 };
