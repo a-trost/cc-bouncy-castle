2884b1128a2edbd6f03fe45edd0a0f4a5ae26152
Alex Trost <alexrtrost@gmail.com>
2021-04-23 10:39:13 -0400

    Add Hero Section Slice

--- slices/index.js
+++ slices/index.js
@@ -1,3 +1,4 @@
 export { default as SignupForm } from './SignupForm'
 export { default as CastleCreator } from './CastleCreator'
 export { default as TeamMembers } from './TeamMembers'
+export { default as HeroSection } from './HeroSection'
